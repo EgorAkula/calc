c581dbc101fa522530ba2fb8a55264dc59c2050f
EgorAkula <147970616+EgorAkula@users.noreply.github.com>
2025-01-18 12:23:17 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,14 +3,22 @@ let input1 = document.getElementById("FirstNumber");
 let input2 = document.getElementById("SecondNumber");
 let output = document.getElementById("result");
 let selector = document.getElementById("Selector");
-let option1 = document.getElementById("FirstOption");
-let option2 = document.getElementById("SecondOption");
-let option3 = document.getElementById("Thirdption");
-let option4 = document.getElementById("FourthOption");
+let option1 = document.getElementById("Plus");
+let option2 = document.getElementById("Minus");
+let option3 = document.getElementById("Multiply");
+let option4 = document.getElementById("Division");
 
 function print() {
     let FirstNumber = Number(input1.value);
     let SecondNumber = Number(input2.value);
-    let result =
-        output.value = result;
+    if (selector == option1) {
+        result = FirstNumber.value + SecondNumber.value;
+    } if (selector == option2) {
+        result = FirstNumber.value - SecondNumber.value;
+    } if (selector == option3) {
+        result = FirstNumber.value * SecondNumber.value;
+    } if (selector == option4) {
+        result = FirstNumber.value / SecondNumber.value
+    }
+    output.value = result;
 }
