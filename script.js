31dedb627cd59567316b1ad3d4ac6820dbde306e
EgorAkula <147970616+EgorAkula@users.noreply.github.com>
2025-01-18 12:05:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let button = document.getElementById("calc");
+let input1 = document.getElementById("FirstNumber");
+let input2 = document.getElementById("SecondNumber");
+let output = document.getElementById("result");
+let selector = document.getElementById("Selector");
+let option1 = document.getElementById("FirstOption");
+let option2 = document.getElementById("SecondOption");
+let option3 = document.getElementById("Thirdption");
+let option4 = document.getElementById("FourthOption");
+
+function print() {
+    let FirstNumber = Number(input1.value);
+    let SecondNumber = Number(input2.value);
+    let result =
+        output.value = result;
+}
